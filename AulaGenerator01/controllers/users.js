5d3534b62c701ebadc7e2e7839dc9b3577f406f6
Marcelo-Diament <marcelodiament@gmail.com>
2020-12-03 22:01:55 -0300

    feature: users initial view

--- AulaGenerator01/controllers/users.js
+++ AulaGenerator01/controllers/users.js
@@ -1,5 +1,8 @@
 const controller = {
-  index: (req, res, next) => res.send(`<h1>Usuários</h1>`),
+  index: (req, res, next) => res.render('users', {
+    title: 'Usuários',
+    subtitle: 'Página da view de Users'
+  }),
   show: (req, res, next) => res.send(`<h1>Usuário ${req.params.id}</h1>`),
   add: (req, res, next) => res.send(`<h1>Adicionar Novo Usuário</h1>`),
   edit: (req, res, next) => res.send(`<h1>Editar Usuário ${req.params.id}</h1>`),
--- AulaGenerator01/routes/index.js
+++ AulaGenerator01/routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  res.render('index', {
+    title: 'Aula de Views',
+    subtitle: 'Aula firmeza bagarai',
+    repo: 'https://github.com/Marcelo-Diament/express-generator'
+  });
 });
 
 module.exports = router;
